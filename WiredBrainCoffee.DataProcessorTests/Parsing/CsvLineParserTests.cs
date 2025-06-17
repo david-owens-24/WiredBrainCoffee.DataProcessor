9f341897e02fee5616bb3bd835a08971a99f649d
David Owens <david.owens24@live.com>
2025-06-17 10:57:27 +0100

    added ShouldSkipEmptyLines unit test and fixed bugs with empty lines in CsvLineParser

--- WiredBrainCoffee.DataProcessorTests/Parsing/CsvLineParserTests.cs
+++ WiredBrainCoffee.DataProcessorTests/Parsing/CsvLineParserTests.cs
@@ -5,10 +5,13 @@
         [Fact]
         public void ShouldParseValidLine()
         {
+            // Arrange
             string[] csvLines = new string[] { "Cappuccino;10/27/2022 8:06:04 AM" };
 
+            // Act
             var machineDataItems = CsvLineParser.Parse(csvLines);
 
+            // Assert
             Assert.NotNull(machineDataItems);
 
             Assert.Single(machineDataItems);
@@ -16,6 +19,21 @@
             Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
 
             Assert.Equal(new DateTime(2022,10,27,8,6,4), machineDataItems[0].CreatedAt);
+        }
+
+        [Fact]
+        public void ShouldSkipEmptyLines()
+        {
+            // Arrange
+            string[] csvLines = new string[] { "", "  " };
+
+            // Act
+            var machineDataItems = CsvLineParser.Parse(csvLines);
+
+            // Assert
+            Assert.NotNull(machineDataItems);
+
+            Assert.Empty(machineDataItems);
         }
     }
 }
