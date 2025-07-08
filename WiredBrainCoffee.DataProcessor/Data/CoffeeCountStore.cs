7b1312826595d708a4694b45afc84f6250433658
David Owens <david.owens24@live.com>
2025-07-08 13:51:42 +0100

    decoupled Console from CoffeeCountStore and added unit test for CoffeeCountStore.Save()

--- WiredBrainCoffee.DataProcessor/Data/CoffeeCountStore.cs
+++ WiredBrainCoffee.DataProcessor/Data/CoffeeCountStore.cs
@@ -4,10 +4,24 @@ namespace WiredBrainCoffee.DataProcessor.Data
 {
     public class ConsoleCoffeeCountStore : ICoffeeCountStore
     {
+        private TextWriter _textWriter;
+
+        // This default constructor passes Console.Out as the TextWriter
+        public ConsoleCoffeeCountStore() : this(Console.Out)
+        {
+            
+        }
+
+        // This allows a different TextWriter to be injected (for example, for testing purposes)
+        public ConsoleCoffeeCountStore(TextWriter textWriter)
+        {
+            _textWriter = textWriter;
+        }
+
         public void Save(CoffeeCountItem item)
         {
             var line = $"{item.CoffeeType}:{item.Count}";
-            Console.WriteLine(line);
+            _textWriter.WriteLine(line);
         }
     }
 }
